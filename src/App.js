e3f40746378b91cd63550e807d2853e59b2ef7ec
joellegg <joellegg@bellsouth.net>
2017-03-11 15:53:28 -0600

    rearranged files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
-import './App.css';
+import './../stylesheets/components/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App App-header">
-        <h1>JOEL Ball LEGG</h1>
+        <h1>JOEL LEGG</h1>
         <h3>Full Stack Software Developer</h3>
         <h3>Petroleum Geologist</h3>
         <h4>Nashville, TN</h4>
-        <image source={require("./profile_bw.jpg")}></img>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+
+import './stylesheets/index.css';
 
 ReactDOM.render(
   <App />,
