9247c04400ed792074927b6308f31d8bf8f0c03f
joellegg <joellegg@bellsouth.net>
2017-03-11 16:59:57 -0600

    installed package to watch sass

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+      <div>
       <div className="App App-header">
         <h1>JOEL LEGG</h1>
-        <h3>Full Stack Software Developer</h3>
-        <h3>Petroleum Geologist</h3>
-        <h4>Nashville, TN</h4>
+        <h5>Full Stack Software Developer</h5>
+        <h5>Petroleum Geologist</h5>
+        <h5>Nashville, TN</h5>
+      </div>
+      <Button
+        bsStyle="success"
+        bsSize="large"
+        href="http://react-bootstrap.github.io/components.html"
+        target="_blank">
+        View React Bootstrap Docs
+      </Button>
       </div>
     );
   }
