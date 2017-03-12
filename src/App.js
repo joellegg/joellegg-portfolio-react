91bfbc7aa342a77cfb2f5cd84301dd6093f8c4ee
joellegg <joellegg@bellsouth.net>
2017-03-12 11:46:54 -0500

    switched to bootstrap 3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React, { Component } from 'react';
-import { Button } from 'react-bootstrap';
+import { ButtonToolbar, ButtonGroup, SplitButton, MenuItem } from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-      <div className="App App-header">
-        <h1>JOEL LEGG</h1>
-        <h5>Full Stack Software Developer</h5>
-        <h5>Petroleum Geologist</h5>
-        <h5>Nashville, TN</h5>
-      </div>
-      <Button
-        bsStyle="success"
-        bsSize="large"
-        href="http://react-bootstrap.github.io/components.html"
-        target="_blank">
-        View React Bootstrap Docs
-      </Button>
+        <div className="App App-header">
+          <h1>JOEL LEGG</h1>
+          <h5>Full Stack Software Developer</h5>
+          <h5>Petroleum Geologist</h5>
+          <h5>Nashville, TN</h5>
+        </div>
+        <ButtonToolbar>
+          <ButtonGroup vertical block>
+            <SplitButton title="Dropup" dropup id="split-button-dropup">
+              <MenuItem eventKey="1">Action</MenuItem>
+              <MenuItem eventKey="2">Another action</MenuItem>
+              <MenuItem eventKey="3">Something else here</MenuItem>
+              <MenuItem divider />
+              <MenuItem eventKey="4">Separated link</MenuItem>
+            </SplitButton>
+          </ButtonGroup>
+        </ButtonToolbar>
+
       </div>
     );
   }
