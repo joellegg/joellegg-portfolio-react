b221e81ed679e65a7c7541847ccaa4f3b5e6e0ed
joellegg <joellegg@bellsouth.net>
2017-04-06 08:41:38 -0500

    updated icons for links on large screen

--- src/components/App.js
+++ src/components/App.js
@@ -5,15 +5,29 @@ function Links() {
   return (
     <div className='links'>
       <div className="links-right">
-        <a href="https://www.linkedin.com/in/joel-legg-279a5b58/"><i className="fa fa-linkedin fa-2x" aria-hidden="true"></i></a>
-        <a href="mailto:joellegg@bellsouth.net"><i className="fa fa-envelope fa-2x" aria-hidden="true"></i></a>
-        <a href="https://github.com/joellegg"><i className="fa fa-github fa-2x" aria-hidden="true"></i></a>
+        <span className='links__circle'>
+          <a href="https://www.linkedin.com/in/joel-legg-279a5b58/"><i className="fa fa-linkedin fa-2x" aria-hidden="true"></i></a>
+        </span>
+        <span className='links__circle'>
+          <a href="mailto:joellegg@bellsouth.net"><i className="fa fa-envelope fa-2x" aria-hidden="true"></i></a>
+        </span>
+        <span className='links__circle'>
+          <a href="https://github.com/joellegg"><i className="fa fa-github fa-2x" aria-hidden="true"></i></a>
+        </span>
       </div>
       <div className="links__lg">
-        <a href="/"><i className="fa fa-linkedin fa-2x" aria-hidden="true"></i></a>
-        <a href="/about"><i className="fa fa-envelope fa-2x" aria-hidden="true"></i></a>
-        <a href="/projects"><i className="fa fa-github fa-2x" aria-hidden="true"></i></a>
-        <a href="/tech"><i className="fa fa-github fa-2x" aria-hidden="true"></i></a>
+        <span className='links__circle'>
+          <a href="/"><i className="fa fa-home fa-2x" aria-hidden="true"></i></a>
+        </span>
+        <span className='links__circle'>
+          <a href="/about"><i className="fa fa-info fa-2x" aria-hidden="true"></i></a>
+        </span>
+        <span className='links__circle'>
+          <a href="/projects"><i className="fa fa-product-hunt fa-2x" aria-hidden="true"></i></a>
+        </span>
+        <span className='links__circle'>
+          <a href="/tech"><i className="fa fa-language fa-2x" aria-hidden="true"></i></a>
+        </span>
       </div>
     </div>
   );
