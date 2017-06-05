95a31bdd01c4e5388a0f082967542eae71c43eb1
joellegg <joellegg@bellsouth.net>
2017-06-05 15:54:39 -0500

    updated resume and added download link, added transparent background on hover with css

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,7 +4,10 @@ class Resume extends Component {
   render() {
     return (
       <div className="Resume__container">
-        <img className="Resume__resume" src={require('../../img/joel_resume.jpg')} alt="joel-resume" />
+        <a href={require('../../img/legg-resume.png')} download="legg-resume">
+          <img className="Resume__download" src={require('../../img/download_icon.png')} />
+        </a>
+        <img className="Resume__resume" src={require('../../img/legg-resume.png')} alt="joel-resume" />
       </div>
     )
   }
