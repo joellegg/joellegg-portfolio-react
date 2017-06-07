aa29b1761dacd91e341780f73147d41980a92555
joellegg <joellegg@bellsouth.net>
2017-06-07 12:51:42 -0500

    rewrote css changes to scss files so deployment will take css changes

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -5,7 +5,7 @@ class Resume extends Component {
     return (
       <div className="Resume__container">
         <a href={require('../../img/legg_resume.pdf')} download="legg-resume">
-          <img className="Resume__download" src={require('../../img/download_icon.png')} />
+          <img className="Resume__download" src={require('../../img/download_icon.png')} alt="download" />
         </a>
         <img className="Resume__resume" src={require('../../img/legg_resume.jpg')} alt="joel-resume" />
       </div>
