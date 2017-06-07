a685360ada1f1fc4e74d4e3df62a8436250fcffa
joellegg <joellegg@bellsouth.net>
2017-06-07 08:58:53 -0500

    added img for well logged

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,6 +6,7 @@ class Projects extends Component {
       <div className="projects_container">
         <div className="project__style">
           <p><strong><a target="_blank" href="https://github.com/joellegg/well-logged-front">Well Logged</a></strong> organizes well logs that are collected during the drilling for oil and gas and applies machine learning techniques to extract text and data from the logs. Each state collects the data but it can be difficult to traverse the various state websites and the data is stored in TIFF format which makes the logs difficult to work with. The project uses React on the front end and SQL for the backend.</p>
+          <img className="Project__images__wl" src={require('../../img/mockups/WellLoggedSS.png')} alt="tool-trader" />
         </div>
         <div className="project__style">
           <div className='Project__inline'>
