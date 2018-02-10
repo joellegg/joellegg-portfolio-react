14343f911d76022027c48c687d77eb5f521d6d04
joellegg <joellegg@bellsouth.net>
2018-02-10 14:56:11 -0600

    updated about section

--- src/components/About.js
+++ src/components/About.js
@@ -9,13 +9,13 @@ class About extends Component {
           <img className="About__funpic" src={require('../../img/joel-mug.jpg')} alt="joel-mug" />
           <div className="About__text">
             <p>
-              I was born and raised in Franklin, TN. I earned a masters degree from the University of Alabama in 2014 and became a published geologist. Afterwards, I worked for a multinational oil company in Denver, CO until circumstances led me back to Nashville, TN and into the field of software development.
+              Hi, I'm Joel, a Nashville native and a full stack software engineer. I've built projects using multiple languages, but I enjoy JavaScript the most (so far!). I hold a masters degree in Geology from the University of Alabama and even worked as a petroleum geologist for several years. 
             </p>
             <p>
-              From discovering the next oil reserve to building the next software program, my passion has always been problem solving. I've worked on problems ranging from figuring out how oil formed millions of years ago to how to drill the next well. Technology was always at the heart of the solution. Software development allows me to combine many of my passions into one field. My goal is to merge my past and present into data science to find insights that can better mankind.
+              From discovering the next oil reserve to building the next software program, my passion has always been problem solving. Software development allows me to combine many of my passions into one field.
             </p>
             <p>
-              When I'm not in front of the computer, I can be found working on a home remodel, woodworking, or tending the garden. I enjoy spending time outdoors, hiking, fishing, golfing, and skiing. Above all, I love to spend time with family and friends.
+              Outside of software development, I'm a husband, new father! and hobbyist woodworker. When possible, I enjoy spending time outdoors, hiking, fishing, golfing, and skiing. 
             </p>
           </div>
         </div>
